70475fea907334490d88222cdfc76fe3a5f8c0db
PlatonB <platon.work@gmail.com>
2020-07-11 21:12:23 +0300

    Программа переименования e-commerce-файлов, V2.0
    - Теперь можно задавать три постоянных компонента имён, располагаемых в начале, посередине и в конце каждого имени.

--- product_renamer.py
+++ product_renamer.py
@@ -1,4 +1,4 @@
-__version__ = 'V1.0'
+__version__ = 'V2.0'
 
 import os, random, time
 from argparse import ArgumentParser, RawTextHelpFormatter
@@ -26,12 +26,16 @@ argparser.add_argument('-S', '--src-dir-path', metavar='str', dest='src_dir_path
                        help='Путь к папке с исходными файлами')
 argparser.add_argument('-t', '--trg-dir-path', metavar='[None]', dest='trg_dir_path', type=str,
                        help='Путь к папке для переименованных файлов (по умолчанию - путь к исходной папке)')
-argparser.add_argument('-d', '--date', dest='date', action='store_true',
+argparser.add_argument('-1', '--first-const-part', metavar='[None]', dest='first_const_part', type=str,
+                       help='Одинаковая для всех файлов начальная часть имени (перед опциональной датой переименования)')
+argparser.add_argument('-d', '--cur-date', dest='cur_date', action='store_true',
                        help='Вписывать в имена файлов дату переименования')
-argparser.add_argument('-n', '--name', metavar='[None]', dest='name', type=str,
-                       help='Общая для всех файлов часть имени')
+argparser.add_argument('-2', '--second-const-part', metavar='[None]', dest='second_const_part', type=str,
+                       help='Одинаковая для всех файлов серединная часть имени (между опциональной датой переименования и артикулом)')
 argparser.add_argument('-a', '--art-digits-quan', metavar='[8]', default=8, dest='art_digits_quan', type=int,
                        help='Количество цифр случайных артикулов')
+argparser.add_argument('-3', '--third-const-part', metavar='[None]', dest='third_const_part', type=str,
+                       help='Одинаковая для всех файлов завершающая часть имени (между артикулом и расширением)')
 args = argparser.parse_args()
 
 if args.trg_dir_path is None:
@@ -46,10 +50,14 @@ print(f'\nПереименование файлов папки {os.path.basename
 
 for src_file_name in os.listdir(args.src_dir_path):
         trg_file_draft = [str(random.randint(minimum, maximum))]
-        if args.date:
+        if args.cur_date:
                 trg_file_draft.insert(0, '_'.join(map(str, time.localtime()[:3])))
-        if args.name != None:
-                trg_file_draft.insert(1, args.name)
+        if args.first_const_part != None:
+                trg_file_draft.insert(0, args.first_const_part)
+        if args.second_const_part != None:
+                trg_file_draft.insert(-1, args.second_const_part)
+        if args.third_const_part != None:
+                trg_file_draft.append(args.third_const_part)
         src_file_ext = src_file_name.split('.')[-1]
         src_file_path = os.path.join(args.src_dir_path, src_file_name)
         trg_file_name = '_'.join(trg_file_draft) + f'.{src_file_ext}'
